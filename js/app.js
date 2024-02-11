0acd2249c0d491e1df301782986b2b43ab5c84f7
Rahikul <rahikuljannat@gmail.com>
2024-02-11 17:43:58 -0500

    storage price calculation done

--- js/app.js
+++ js/app.js
@@ -27,6 +27,8 @@ function extraMemoryPrice(memory){
 
 //storage price
 let storageAddedOnce = false;
+let selectedStorage= "";
+let basePrice= 1299;
 
 function extraStoragePrice(storage){
     const extraStorage = document.getElementById('storage-price');
@@ -35,16 +37,24 @@ function extraStoragePrice(storage){
      const totalPrice = document.getElementById('total-price');
      const newTotalPrice = parseInt(totalPrice.innerText);
 
+     if(selectedStorage === "storage-2"){
+         totalPrice.innerText -= 60;
+     }
+     else if(selectedStorage === "storage-3"){
+         totalPrice.innerText -= 100;
+     }
+
      if (storage === "storage-1") {
         extraStorage.innerText = "0"; // Set storage price to 0
-        totalPrice.innerText = newTotalPrice - addExtraStorage;
-        storageAddedOnce = false; // Reset storageAddedOnce flag
+
     } else if (storage === "storage-2") {
         extraStorage.innerText = "60"; // Display 60 in storage-price
-        totalPrice.innerText = newTotalPrice + 60;
+        totalPrice.innerText = basePrice + 60;
+       
     } else if (storage === "storage-3") {
         extraStorage.innerText = "100"; // Display 100 in storage-price
-        totalPrice.innerText = newTotalPrice - 60 + 100; // Remove previous value (60) and add 100
+        totalPrice.innerText = basePrice + 100; // Remove previous value (60) and add 100
+      
     }
 
           //total summary
@@ -54,7 +64,7 @@ function extraStoragePrice(storage){
               otherTotalPriceElement.innerText = totalPrice.innerText;
           }
 
-     
+          selectedStorage = storage;
 }
 
 //memory price
