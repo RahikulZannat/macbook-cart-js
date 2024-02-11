0f921e811ec17f47698e2765a997854d811eae54
Rahikul <rahikuljannat@gmail.com>
2024-02-11 02:51:44 -0500

    update js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
+let memoryAddedOnce = false;
+function extraMemoryPrice(memory){
+    const extraMemory = document.getElementById('memory-price');
+    const addExtraMemory = parseInt(extraMemory.innerText);
+    // total price
+    const totalPrice = document.getElementById('total-price');
+    const newTotalPrice = parseInt(totalPrice.innerText);
+
+
+    if(memory && !memoryAddedOnce){
+        extraMemory.innerText = addExtraMemory + 20;
+        totalPrice.innerText = newTotalPrice + 20;
+        memoryAddedOnce = true;
+    }
+    else if(!memory && memoryAddedOnce){
+        extraMemory.innerText = addExtraMemory -20;
+        totalPrice.innerText = newTotalPrice - 20;
+        memoryAddedOnce = false;
+    }
+}
+
+//memory price
 document.getElementById('memory-1').addEventListener('click',function(){
-    console.log('click');
+    extraMemoryPrice(false);
+})
+document.getElementById('memory-2').addEventListener('click',function(){
+    extraMemoryPrice(true);
 })
